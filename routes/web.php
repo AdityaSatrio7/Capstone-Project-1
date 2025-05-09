2edf61ac5acee8d262d80f2d46d1bd6020170af5
Aditya I. Satrio <adityaisatrio404@gmail.com>
2025-05-09 22:56:12 +0700

    Initial First Update

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-
-//route resource for products
-Route::resource('/products', \App\Http\Controllers\ProductController::class);
